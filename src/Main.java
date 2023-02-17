091618d73ef4e7d5d4e058719ece3d32c59c3682
charlie d <charlie@pop-os.localdomain>
2023-02-16 20:41:51 -0600

    Added creation of 'freq' data structure for first step of datafly algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * How to build freq:
@@ -23,9 +19,41 @@ import java.util.List;
  */
 
 public class Main {
-    public static void main(String[] args) {
 
-//        List<List<String>> records = new ArrayList<>();
+    public static int dataFly(ArrayList<Person> people) {
+       ArrayList<Field2> freq = new ArrayList<>();
+
+       DataType[] dataMembers = {DataType.AGE, DataType.WORKCLASS, DataType.EDUCATION, DataType.EDUCATION_NUM, DataType.MARITAL_STATUS, DataType.OCCUPATION, DataType.RELATION, DataType.RACE, DataType.SEX, DataType.CAPITAL_GAINS, DataType.CAPITAL_LOSS, DataType.HOURS_PER_WEEK, DataType.NATIVE_COUNTRY};
+
+       int pos = 0;
+       int total = 0;
+       for (DataType dt : dataMembers){
+           for (Person p : people) {
+               Field2 f;
+               if (dt == DataType.AGE) {
+                   f = new Field2(p.getStr_age(), p.getSid());
+               } else if (dt == DataType.EDUCATION) {
+                   f = new Field2(p.getEducation(), p.getSid());
+               } else if (dt == DataType.MARITAL_STATUS) {
+                   f = new Field2(p.getMaritalStatus(), p.getSid());
+               } else if (dt == DataType.RACE) {
+                   f = new Field2(p.getRace(), p.getSid());
+               } else continue;
+                   boolean found = false;
+                   for (Field2 inFreq : freq) {
+                       if (f.getData().equals(inFreq.getData())) {
+                           inFreq.increaseCount(p.getSid());
+                           found = true;
+                           break;
+                       }
+                   }
+                   if (!found) freq.add(f);
+               }
+       }
+       return 0;
+   }
+   public static void main(String[] args) {
+
         ArrayList<Person> people = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("adult.data"))) {
             int sid = 0;
@@ -34,11 +62,11 @@ public class Main {
                 Person p = new Person(line, sid);
                 people.add(p);
                 sid++;
-//                records.add(Arrays.asList(values));
             }
         } catch (Exception e) {
 
         }
+        dataFly(people);
         System.out.println("DONE");
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 
 public class Person {
     private int sid;
+
+    // cha added
+    private String str_age;
     private int age;
     private String workClass;
     private int fnlwgt;
@@ -23,6 +26,9 @@ public class Person {
 
         this.sid = sid;
 
+        // cha added
+        str_age = values[0].trim();
+
         age = Integer.parseInt(values[0].trim());
         workClass = values[1].trim();
         fnlwgt = Integer.parseInt(values[2].trim());
@@ -37,5 +43,69 @@ public class Person {
         capital_loss = Integer.parseInt(values[11].trim());
         hours_per_week = Integer.parseInt(values[12].trim());
         native_country = values[13].trim();
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    // cha added
+    public String getStr_age() {
+        return str_age;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public String getWorkClass() {
+        return workClass;
+    }
+
+    public int getFnlwgt() {
+        return fnlwgt;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public int getEducationNum() {
+        return educationNum;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public String getRelationship() {
+        return relationship;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public int getCapital_gain() {
+        return capital_gain;
+    }
+
+    public int getCapital_loss() {
+        return capital_loss;
+    }
+
+    public int getHours_per_week() {
+        return hours_per_week;
+    }
+
+    public String getNative_country() {
+        return native_country;
     }
 }
